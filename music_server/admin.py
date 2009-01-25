47afa896bd72151aa8ffd4c9186f1dff7ad58465
Chris Lamb <lamby@debian.org>
2009-01-25 20:45:17 +0000

    Remove admin.py.
    Signed-off-by: Chris Lamb <lamby@debian.org>

--- music_server/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from music_server.models import Item, YouTubeQueue
-
-class ItemAdmin(admin.ModelAdmin): pass
-admin.site.register(Item, ItemAdmin)
-
-class YouTubeQueueAdmin(admin.ModelAdmin): pass
-admin.site.register(YouTubeQueue, YouTubeQueueAdmin)
